8cd44c8423ad1cf25d36c885d6e2ec9f393bef54
Alejandro Cazorla <acazorla72@gmail.com>
2021-11-06 16:59:24 +0100

    added profile route

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -2,7 +2,14 @@ const router = require("express").Router();
 const User = require("../models/User.model");
 const Thread = require("../models/Thread.model");
 const Product = require("../models/Product.model");
+const { isLoggedIn } = require("../middlewares/authorization");
 
-router.get("/", (req, res, next) => {});
+router.get("/", isLoggedIn, (req, res, next) => {
+  User.findById(req.session.user._id)
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => console.log(err));
+});
 
 module.exports = router;
