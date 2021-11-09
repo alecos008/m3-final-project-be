63471fba7a8724cf41681787a5913c75b3c0b30d
nicoafd <dottinick@gmail.com>
2021-11-09 19:54:16 +0100

    added product route display product by ID

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,19 +1,20 @@
 const router = require("express").Router();
+const { isLoggedIn } = require("../middlewares/authorization");
 const Product = require("../models/Product.model");
 
 // CREATE A PRODUCT
-router.post("/create", (req, res, next) => {
-  const { name, image, description, price, category, stock, addedBy } =
+router.post("/create", isLoggedIn, (req, res, next) => {
+  const { name, description, price, category, stock } =
     req.body;
 
   Product.create({
     name,
-    image,
+  
     description,
     price,
     category,
     stock,
-    addedBy,
+    addedBy: req.session.user._id
   })
     .then((product) => {
       console.log(`Here is the new product: ${product}`);
@@ -29,6 +30,13 @@ router.post("/create", (req, res, next) => {
 // DISPLAY ALL PRODUCTS
 router.get("/all", (req, res, next) => {
   Product.find()
+    .then((data) => res.json(data))
+    .catch((err) => next(err));
+});
+
+// DISPLAY SINGLE PRODUCT BY ID
+router.get("/:id", (req, res, next) => {
+  Product.findById(req.params.id)
     .then((data) => res.json(data))
     .catch((err) => next(err));
 });
