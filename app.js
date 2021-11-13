1fa16b689a24d97fd2ba534acdec18ff010531dc
Alejandro Cazorla <acazorla72@gmail.com>
2021-11-13 15:08:19 +0100

    deployment setup

--- app.js
+++ app.js
@@ -14,10 +14,18 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controlled from the routes/index.js
 const allRoutes = require("./routes");
 app.use("/api", allRoutes);
+
+app.use((req, res, next) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
