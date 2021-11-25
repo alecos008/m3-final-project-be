8a1c6abfd5af8121b1121f588f1fd0623b011310
nicoafd <dottinick@gmail.com>
2021-11-25 18:44:55 +0100

    payment

--- routes/payments.routes.js
+++ routes/payments.routes.js
@@ -36,4 +36,13 @@ router.post("/create-payment-intent", async (req, res) => {
   });
 });
 
+router.get("/order/success", async (req, res) => {
+  const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
+  const customer = await stripe.customers.retrieve(session.customer);
+
+  res.send(
+    `<html><body><h1>Thanks for your order, ${customer.name}!</h1></body></html>`
+  );
+});
+
 module.exports = router;
